7fed16f79def969b9802277d5fd62341b1044c3f
nullbr <bmarianoleite3@gmail.com>
2022-07-04 08:58:51 -0300

    singleton class

---
+++ logger/test_singleton.py
@@ -0,0 +1,13 @@
+from tokenize import Single
+from singleron_object import SingletonObject
+
+obj1 = SingletonObject()
+
+obj1.val = "Object value 1"
+print("print obj1: ", obj1)
+
+print("----")
+obj2 = SingletonObject()
+obj2.val = "Object value 2"
+print("print obj1: ", obj1)
+print("print obj2: ", obj2)
