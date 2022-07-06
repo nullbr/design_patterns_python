31c4f2b0317e782ee334ad682cb2bdefebae3589
nullbr <bmarianoleite3@gmail.com>
2022-07-06 08:00:06 -0300

    understanding the pattern

--- 1_singleton_pattern/singleton_logger/test_singleton_logger.py
+++ 1_singleton_pattern/singleton_logger/test_singleton_logger.py
@@ -2,10 +2,15 @@ from tokenize import Single
 from singleton_logger import Logger
 
 logger_object = Logger()
-logger_object.filename = "./singleton_logger/script.log"
+logger_object.filename = "./1_singleton_pattern/singleton_logger/script.log"
 
 logger_object.info("This is an info message")
 
 print("Logger Object: ", logger_object)
 
-logger_object.warn("THIS IS A WARN MESSAGE!!")
+
+logger_object2 = Logger()
+
+logger_object2.warn("THIS IS A WARN MESSAGE!!")
+
+print("Logger Object 2: ", logger_object2)
